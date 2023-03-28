483fe95106990b26982f38a4ba21011cc6d42fdf
vanguardDev2 <129090473+vanguardDev2@users.noreply.github.com>
2023-03-27 22:32:05 -0500

    confirmo si soy

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
+
+
 const btnHamburger = document.querySelector(".hamburger"),
       items = document.querySelector(".menu__items")
 
 btnHamburger.addEventListener("click", () => {
     btnHamburger.classList.toggle("is-active")
     items.classList.toggle("is-active")
-})
+
+    
+    
+}
+
+
+
+)
